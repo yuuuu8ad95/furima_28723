e9fe941ef4ba8a922f7269e5df200a9500b1aef7
yuuuu8ad95 <yu.i08elle@icloud.com>
2020-08-30 23:33:44 +0900

    devise標準機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :nickname, :first_name, :family_name, :first_name_kana, :family_name_kana, :birthday ,presence: true
 end
