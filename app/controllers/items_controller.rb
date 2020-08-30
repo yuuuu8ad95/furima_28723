29b145cba390fdad6942d1d53393246c0b64e794
yuuuu8ad95 <yu.i08elle@icloud.com>
2020-08-30 18:03:42 +0900

    ユーザー管理機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,4 @@
-class ItemsController < ApplicationController
+class ItemsController < ApplicationController 
   def index
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'items/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "items#index"
+  root to: 'items#index'
+  
+
+
 end
+
