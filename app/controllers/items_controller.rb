47a588670038cf4227c6732a74c20063b1bc13f1
yuuuu8ad95 <yu.i08elle@icloud.com>
2020-09-04 23:56:28 +0900

    Active Storageの導入2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -12,4 +12,9 @@ class ItemsController < ApplicationController
       redirect_to action: :index
     end
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
 end
