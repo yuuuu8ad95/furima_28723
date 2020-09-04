5886fd4877276cd7b18fb4c79f276fbc9115fb13
yuuuu8ad95 <yu.i08elle@icloud.com>
2020-09-04 15:40:51 +0900

    商品出品ページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,15 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
   def index
   end
 
   def new
     @item = Item.new
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
