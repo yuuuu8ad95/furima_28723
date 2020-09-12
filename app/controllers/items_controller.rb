65555be4df27db12017db0b87c58ed525a78dbe6
yuuuu8ad95 <yu.i08elle@icloud.com>
2020-09-12 11:13:58 +0900

    不要なコード削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,15 +3,10 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @items = Item.order('created_at DESC')
   end
 
   def new
     @item = Item.new
-  end
-
-  def show
-    @item = Item.find(params[:id])
   end
 
   def create
