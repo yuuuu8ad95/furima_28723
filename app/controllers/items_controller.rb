08916b3f080c56a81953b5829a7bfc69ac5be040
yuuuu8ad95 <yu.i08elle@icloud.com>
2020-09-12 22:17:22 +0900

    出品した商品の一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,8 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
+    @items = Item.all
+    @items = Item.order('created_at DESC')
   end
 
   def new
@@ -16,6 +18,10 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
+    @item = Item.find(params[:id])
   end
 
   private
