9e66aad8030be3695ca6479bb96ec742e593b1f2
yuuuu8ad95 <yu.i08elle@icloud.com>
2020-09-13 22:23:12 +0900

    ログアウト状態でも閲覧可能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only:[:show]
 
   def index
