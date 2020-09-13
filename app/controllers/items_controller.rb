e030d9497b9d9c7db4698c0a6e3962fe78ca585c
yuuuu8ad95 <yu.i08elle@icloud.com>
2020-09-13 16:51:44 +0900

    Items_controllerの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
 
   def index
-    @items = Item.all
-    @items = Item.order('created_at DESC')
+    @items = Item.all.order('created_at DESC')
   end
 
   def new
