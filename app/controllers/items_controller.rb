66470039d0e4a4d9588cbcef2bc68b7f2f3473e5
yuuuu8ad95 <yu.i08elle@icloud.com>
2020-09-14 11:29:03 +0900

    商品情報の表示と変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,6 +27,16 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   def order
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: "items#index"
   devise_for :users
-  resources :items, only:[:index, :new, :create, :show, :destroy, :edit] 
+  resources :items
   resources :items, only: :order do
       get 'order', on: :member
     end
