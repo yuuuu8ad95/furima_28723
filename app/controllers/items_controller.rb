bb0b6f7da02f38c2d4949407bae7e774468d0938
yuuuu8ad95 <yu.i08elle@icloud.com>
2020-09-15 15:15:21 +0900

    jsファイルの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -43,6 +43,22 @@ class ItemsController < ApplicationController
   end
 
   def order
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    customer = Payjp::Customer.create(
+    description: 'test', 
+    card: params[:card_token]
+    )
+
+  item = Item.new(
+       card_token: params[:card_token],
+       customer_token: customer.id,
+       user_id: current_user.id
+    )
+    if card.save
+      redirect_to root_path
+    else
+      redirect_to "new"
+    end
   end
 
   private
