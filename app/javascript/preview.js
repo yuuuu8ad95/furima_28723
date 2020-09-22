6e8ad1e4fad391df7aa04c967863587720f7871d
yuuuu8ad95 <yu.i08elle@icloud.com>
2020-09-22 13:44:53 +0900

    複数枚画像投稿の設定

--- app/javascript/preview.js
+++ app/javascript/preview.js
@@ -1,28 +1,38 @@
 if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
   document.addEventListener('DOMContentLoaded', function(){
-    const ImageList = document.getElementById('image-list');
+    const ImageList = document.getElementById('image-list')
 
     const createImageHTML = (blob) => {
-      const imageElement = document.createElement('div');
+      const imageElement = document.createElement('div')
+      imageElement.setAttribute('id', "image-element")
+      let imageElementNum = document.querySelectorAll('#image-element').length
 
-      const blobImage = document.createElement('img');
-      blobImage.setAttribute('src', blob);
+      const blobImage = document.createElement('img')
+      blobImage.setAttribute('src', blob)
 
-      imageElement.appendChild(blobImage);
-      ImageList.appendChild(imageElement);
-    };
+      const inputHTML = document.createElement('input')
+      inputHTML.setAttribute('id', `item-image_${imageElementNum}`)
+      inputHTML.setAttribute('name', 'item[images][]')
+      inputHTML.setAttribute('type', 'file')
+
+      imageElement.appendChild(blobImage)
+      imageElement.appendChild(inputHTML)
+      ImageList.appendChild(imageElement)
+
+      inputHTML.addEventListener('change', (e) => {
+        file = e.target.files[0];
+        blob = window.URL.createObjectURL(file);
+
+        createImageHTML(blob)
+      })
+    }
 
     document.getElementById('item-image').addEventListener('change', function(e){
+      let file = e.target.files[0];
+      let blob = window.URL.createObjectURL(file);
 
-      const imageContent = document.querySelector('img');
-      if (imageContent){
-        imageContent.remove();
-      }
+      createImageHTML(blob)
 
-      const file = e.target.files[0];
-      const blob = window.URL.createObjectURL(file);
-
-      createImageHTML(blob);
     });
   });
 }
